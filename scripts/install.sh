ea67f566c2a9bce7a436f6e06bee6af91f468d1a
egmcbride <ethan.mcbride@alleninstitute.org>
2023-07-26 00:07:44 +0000

    add xarray dependency

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,6 @@ python -m pip install pipx
 pipx install pdm
 dirname=${PWD##*/} 
 pyversion=$(python -V 2>&1 | grep -Po '(?<=Python )(.+)')
-python -m venv .venv --without-pip --prompt "$dirname-$pyversion"
+python -m venv .venv --copies --without-pip --prompt "$dirname-$pyversion"
 source .venv/bin/activate
 pdm install
