fe31051e222816401e5d4eb52343b851b4eca875
egmcbride <ethan.g.mcbride@gmail.com>
2024-05-20 12:56:30 -0700

    add linear shift to single unit metrics

--- src/dynamic_routing_analysis/spike_utils.py
+++ src/dynamic_routing_analysis/spike_utils.py
@@ -1,10 +1,11 @@
 import os
+
 import matplotlib.pyplot as plt
 import npc_lims
 import numpy as np
 import pandas as pd
-import xarray as xr
 import scipy.stats as st
+import xarray as xr
 from sklearn.metrics import roc_auc_score
 
 #functions for making 3d trial-aligned tensor
@@ -327,6 +328,12 @@ def compute_stim_context_modulation(trials, units, session_info, save_path):
         'baseline_context_modulation_zscore':[],
         'baseline_context_modulation_sign':[],
         'baseline_context_roc_auc':[],
+        'linear_shift_baseline_context_true_value':[],
+        'linear_shift_baseline_context_null_median':[],
+        'linear_shift_baseline_context_null_mean':[],
+        'linear_shift_baseline_context_null_std':[],
+        'linear_shift_baseline_context_p_value_higher':[],
+        'linear_shift_baseline_context_p_value_lower':[],
         'vis_discrim_roc_auc':[],
         'aud_discrim_roc_auc':[],
         'target_discrim_roc_auc':[],
@@ -399,6 +406,22 @@ def compute_stim_context_modulation(trials, units, session_info, save_path):
     binsize = 0.025
     trial_da = make_neuron_time_trials_tensor(units, trials, time_before, time_after, binsize)
 
+    #get linear shifts
+    #find middle 4 block labels
+    first_block=trials.query('block_index==0').index.values
+    middle_of_first=first_block[np.round(len(first_block)/2).astype('int')]
+
+    last_block=trials.query('block_index==5').index.values
+    middle_of_last=last_block[np.round(len(last_block)/2).astype('int')]
+
+    middle_4_block_trials=trials.loc[middle_of_first:middle_of_last]
+    middle_4_blocks=middle_4_block_trials.index.values
+
+    #find the number of trials to shift by, from -1 to +1 block
+    negative_shift=middle_4_blocks.min()
+    positive_shift=trials.index.max()-middle_4_blocks.max()
+    shifts=np.arange(-negative_shift,positive_shift+1)
+
     #for each unit
     for uu,unit in units.iterrows():
 
@@ -431,6 +454,29 @@ def compute_stim_context_modulation(trials, units, session_info, save_path):
         binary_label=trials['context_name']=='vis'
         baseline_context_auc=roc_auc_score(binary_label,baseline_frs.values)
         stim_context_modulation['baseline_context_roc_auc'].append(baseline_context_auc)
+
+        #linear shifted baseline context
+        temp_baseline_context_diff=[]
+        for sh,shift in enumerate(shifts):
+            labels = middle_4_block_trials['context_name'].values
+            input_data = baseline_frs.sel(trials=middle_4_blocks+shift).values
+            temp_baseline_context_diff.append(input_data[labels=='vis'].mean()-input_data[labels=='aud'].mean())
+        temp_baseline_context_diff=np.asarray(temp_baseline_context_diff)
+        
+        true_value = temp_baseline_context_diff[shifts==1]
+        null_median = np.median(temp_baseline_context_diff[shifts!=1])
+        null_mean = np.mean(temp_baseline_context_diff[shifts!=1])
+        null_std = np.std(temp_baseline_context_diff[shifts!=1])
+        pval_higher = np.mean(temp_baseline_context_diff[shifts!=1]>=true_value)
+        pval_lower = np.mean(temp_baseline_context_diff[shifts!=1]<=true_value)
+
+        stim_context_modulation['linear_shift_baseline_context_true_value'].append(true_value)
+        stim_context_modulation['linear_shift_baseline_context_null_median'].append(null_median)
+        stim_context_modulation['linear_shift_baseline_context_null_mean'].append(null_mean)
+        stim_context_modulation['linear_shift_baseline_context_null_std'].append(null_std)
+        stim_context_modulation['linear_shift_baseline_context_p_value_higher'].append(pval_higher)
+        stim_context_modulation['linear_shift_baseline_context_p_value_lower'].append(pval_lower)
+
 
         #cross stimulus discrimination
         #vis1 vs. vis2
@@ -626,3 +672,285 @@ def compute_stim_context_modulation(trials, units, session_info, save_path):
     unit_metric_merge=units.reset_index(drop=True).merge(pd.DataFrame(stim_context_modulation),on=['unit_id','session_id'])
     unit_metric_merge=unit_metric_merge.drop(columns=['spike_times'])
     unit_metric_merge.to_pickle(os.path.join(save_path,session_info.id+'_stim_context_modulation.pkl'))
+
+
+#calculate metrics for channel alignment
+
+def compute_metrics_for_alignment(trials, units, session_info, save_path):
+
+
+
+    alignment_metrics = {
+        'unit_id':[],
+        'session_id':[],
+        'experiment_day':[],
+        'project':[],
+        'probe':[],
+        'peak_channel':[],
+        'lick_modulation_roc_auc':[],
+        'vis_discrim_roc_auc':[],
+        'aud_discrim_roc_auc':[],
+        'any_vis_roc_auc':[],
+        'any_aud_roc_auc':[],
+        'firing_rate':[],
+        'peak_to_valley':[],
+        'peak_trough_ratio':[],
+        'repolarization_slope':[],
+        'recovery_slope':[],
+        'spread':[],
+        'velocity_above':[],
+        'velocity_below':[],
+        'snr':[],
+
+        'amplitude_cutoff':[], 
+        'amplitude_cv_median':[], 
+        'amplitude_cv_range':[],
+        'amplitude_median':[], 
+        'drift_ptp':[], 
+        'drift_std':[], 
+        'drift_mad':[],
+        'firing_range':[], 
+        'isi_violations_ratio':[],
+        'isi_violations_count':[], 
+        'presence_ratio':[],
+        'rp_contamination':[], 
+        'rp_violations':[], 
+        'sliding_rp_violation':[],
+        'sync_spike_2':[], 
+        'sync_spike_4':[], 
+        'sync_spike_8':[], 
+        'd_prime':[],
+        'isolation_distance':[], 
+        'l_ratio':[], 
+        'silhouette':[], 
+        'nn_hit_rate':[],
+        'nn_miss_rate':[], 
+        'exp_decay':[], 
+        'half_width':[], 
+        'num_negative_peaks':[],
+        'num_positive_peaks':[],
+    }
+
+    if trials is not None:
+        contexts=trials['context_name'].unique()
+
+        if 'Templeton' in session_info.project:
+            contexts = ['aud','vis']
+
+            start_time=trials['start_time'].iloc[0]
+            fake_context=np.full(len(trials), fill_value='nan')
+            fake_block_nums=np.full(len(trials), fill_value=np.nan)
+
+            if np.random.choice(contexts,1)=='vis':
+                block_contexts=['vis','aud','vis','aud','vis','aud']
+            else:
+                block_contexts=['aud','vis','aud','vis','aud','vis']
+
+            trials['true_block_index']=trials['block_index']
+            trials['true_context_name']=trials['context_name']
+
+            for block in range(0,6):
+                block_start_time=start_time+block*10*60
+                block_end_time=start_time+(block+1)*10*60
+                block_trials=trials.query('start_time>=@block_start_time').index
+                fake_context[block_trials]=block_contexts[block]
+                fake_block_nums[block_trials]=block
+            
+            trials['context_name']=fake_context
+            trials['block_index']=fake_block_nums
+            trials['is_vis_context']=trials['context_name']=='vis'
+            trials['is_aud_context']=trials['context_name']=='aud'
+
+        #make data array first
+        time_before = 0.5
+        time_after = 0.5
+        binsize = 0.025
+        trial_da = make_neuron_time_trials_tensor(units, trials, time_before, time_after, binsize)
+
+    #for each unit
+    for uu,unit in units.iterrows():
+        
+        alignment_metrics['unit_id'].append(unit['unit_id'])
+        alignment_metrics['session_id'].append(str(session_info.id))
+        alignment_metrics['project'].append(str(session_info.project))
+        alignment_metrics['experiment_day'].append(str(session_info.experiment_day))
+        alignment_metrics['probe'].append(str(unit['electrode_group_name']))
+        alignment_metrics['peak_channel'].append(unit['peak_channel'])
+
+        alignment_metrics['firing_rate'].append(unit['firing_rate'])
+        alignment_metrics['peak_to_valley'].append(unit['peak_to_valley'])
+        alignment_metrics['peak_trough_ratio'].append(unit['peak_trough_ratio'])
+        alignment_metrics['repolarization_slope'].append(unit['repolarization_slope'])
+        alignment_metrics['recovery_slope'].append(unit['recovery_slope'])
+
+        alignment_metrics['spread'].append(unit['spread'])
+        alignment_metrics['velocity_above'].append(unit['velocity_above'])
+        alignment_metrics['velocity_below'].append(unit['velocity_below'])
+        alignment_metrics['snr'].append(unit['spread'])
+
+        alignment_metrics['amplitude_cutoff'].append(unit['amplitude_cutoff'])
+        alignment_metrics['amplitude_cv_median'].append(unit['amplitude_cv_median'])
+        alignment_metrics['amplitude_cv_range'].append(unit['amplitude_cv_range'])
+        alignment_metrics['amplitude_median'].append(unit['amplitude_median'])
+        alignment_metrics['drift_ptp'].append(unit['drift_ptp'])
+        alignment_metrics['drift_std'].append(unit['drift_std'])
+        alignment_metrics['drift_mad'].append(unit['drift_mad'])
+        alignment_metrics['firing_range'].append(unit['firing_range'])
+        alignment_metrics['isi_violations_ratio'].append(unit['isi_violations_ratio'])
+        alignment_metrics['isi_violations_count'].append(unit['isi_violations_count'])
+        alignment_metrics['presence_ratio'].append(unit['presence_ratio'])
+        alignment_metrics['rp_contamination'].append(unit['rp_contamination'])
+        alignment_metrics['rp_violations'].append(unit['rp_violations'])
+        alignment_metrics['sliding_rp_violation'].append(unit['sliding_rp_violation'])
+        alignment_metrics['sync_spike_2'].append(unit['sync_spike_2'])
+        alignment_metrics['sync_spike_4'].append(unit['sync_spike_4'])
+        alignment_metrics['sync_spike_8'].append(unit['sync_spike_8'])
+        alignment_metrics['d_prime'].append(unit['d_prime'])
+        alignment_metrics['isolation_distance'].append(unit['isolation_distance'])
+        alignment_metrics['l_ratio'].append(unit['l_ratio'])
+        alignment_metrics['silhouette'].append(unit['silhouette'])
+        alignment_metrics['nn_hit_rate'].append(unit['nn_hit_rate'])
+        alignment_metrics['nn_miss_rate'].append(unit['nn_miss_rate'])
+        alignment_metrics['exp_decay'].append(unit['exp_decay'])
+        alignment_metrics['half_width'].append(unit['half_width'])
+        alignment_metrics['num_negative_peaks'].append(unit['num_negative_peaks'])
+        alignment_metrics['num_positive_peaks'].append(unit['num_positive_peaks'])
+        
+        #if surface channel, don't try to calculate metrics
+        if unit['peak_channel']>383:
+            #append nans for all metrics
+            alignment_metrics['any_vis_roc_auc'].append(np.nan)
+            alignment_metrics['any_aud_roc_auc'].append(np.nan)
+            alignment_metrics['vis_discrim_roc_auc'].append(np.nan)
+            alignment_metrics['aud_discrim_roc_auc'].append(np.nan)
+            alignment_metrics['lick_modulation_roc_auc'].append(np.nan)
+            continue
+        
+        if trials is not None:
+            #find baseline frs across all trials
+            baseline_frs = trial_da.sel(unit_id=unit['unit_id'],time=slice(-0.1,0)).mean(dim='time')
+
+            all_stim_frs_by_trial = {}
+            #loop through stimuli
+            for ss in trials['stim_name'].unique():
+
+                #stimulus modulation
+                if "Templeton" in session_info.project:
+                    stim_trials = trials.query('stim_name==@ss')
+                else:
+                    stim_trials = trials.query('stim_name==@ss and is_response==False') #remove response trials to minimize contamination
+                stim_frs_by_trial = trial_da.sel(unit_id=unit['unit_id'],time=slice(0,0.1),trials=stim_trials.index).mean(dim='time',skipna=True)
+
+                all_stim_frs_by_trial[ss]=stim_frs_by_trial
+
+            if "Templeton" in session_info.project:
+                any_vis_trials = trials.query('stim_name.str.contains("vis")')
+            else:
+                any_vis_trials = trials.query('stim_name.str.contains("vis") and is_response==False')
+            any_vis_frs_by_trial = trial_da.sel(unit_id=unit['unit_id'],time=slice(0,0.1),trials=any_vis_trials.index).mean(dim='time',skipna=True)
+            any_vis_baseline_frs_by_trial = baseline_frs.sel(trials=any_vis_trials.index)
+            any_vis_and_baseline_frs=np.concatenate([any_vis_frs_by_trial.values,any_vis_baseline_frs_by_trial.values])
+            binary_label=np.concatenate([np.ones(len(any_vis_frs_by_trial)),np.zeros(len(any_vis_baseline_frs_by_trial))])
+            if len(np.unique(binary_label))>1:
+                any_vis_context_auc=roc_auc_score(binary_label,any_vis_and_baseline_frs)
+            else:
+                any_vis_context_auc=np.nan
+            alignment_metrics['any_vis_roc_auc'].append(any_vis_context_auc)
+
+            if "Templeton" in session_info.project:
+                any_aud_trials = trials.query('stim_name.str.contains("sound")')
+            else:
+                any_aud_trials = trials.query('stim_name.str.contains("sound") and is_response==False')
+            any_aud_frs_by_trial = trial_da.sel(unit_id=unit['unit_id'],time=slice(0,0.1),trials=any_aud_trials.index).mean(dim='time',skipna=True)
+            any_aud_baseline_frs_by_trial = baseline_frs.sel(trials=any_aud_trials.index)
+            any_aud_and_baseline_frs=np.concatenate([any_aud_frs_by_trial.values,any_aud_baseline_frs_by_trial.values])
+            binary_label=np.concatenate([np.ones(len(any_aud_frs_by_trial)),np.zeros(len(any_aud_baseline_frs_by_trial))])
+            if len(np.unique(binary_label))>1:
+                any_aud_context_auc=roc_auc_score(binary_label,any_aud_and_baseline_frs)
+            else:
+                any_aud_context_auc=np.nan
+            alignment_metrics['any_aud_roc_auc'].append(any_aud_context_auc)
+
+            #same modality stimulus discrimination
+            #vis1 vs. vis2
+            vis1_and_vis2_frs=np.concatenate([all_stim_frs_by_trial['vis1'].values,all_stim_frs_by_trial['vis2'].values])
+            binary_label=np.concatenate([np.ones(len(all_stim_frs_by_trial['vis1'])),np.zeros(len(all_stim_frs_by_trial['vis2']))])
+            if len(np.unique(binary_label))>1:
+                vis_discrim_auc=roc_auc_score(binary_label,vis1_and_vis2_frs)
+            else:
+                vis_discrim_auc=np.nan
+            alignment_metrics['vis_discrim_roc_auc'].append(vis_discrim_auc)
+
+            #aud1 vs. aud2
+            aud1_and_aud2_frs=np.concatenate([all_stim_frs_by_trial['sound1'].values,all_stim_frs_by_trial['sound2'].values])
+            binary_label=np.concatenate([np.ones(len(all_stim_frs_by_trial['sound1'])),np.zeros(len(all_stim_frs_by_trial['sound2']))])
+            if len(np.unique(binary_label))>1:
+                aud_discrim_auc=roc_auc_score(binary_label,aud1_and_aud2_frs)
+            else:
+                aud_discrim_auc=np.nan
+            alignment_metrics['aud_discrim_roc_auc'].append(aud_discrim_auc)
+
+            # #targets: vis1 vs sound1
+            # vis1_vs_aud1_frs=np.concatenate([all_stim_frs_by_trial['vis1'].values,all_stim_frs_by_trial['sound1'].values])
+            # binary_label=np.concatenate([np.ones(len(all_stim_frs_by_trial['vis1'])),np.zeros(len(all_stim_frs_by_trial['sound1']))])
+            # target_discrim_auc=roc_auc_score(binary_label,vis1_vs_aud1_frs)
+            # alignment_metrics['target_discrim_roc_auc'].append(target_discrim_auc)
+
+            # #nontargets: vis2 vs sound2
+            # vis2_vs_aud2_frs=np.concatenate([all_stim_frs_by_trial['vis2'].values,all_stim_frs_by_trial['sound2'].values])
+            # binary_label=np.concatenate([np.ones(len(all_stim_frs_by_trial['vis2'])),np.zeros(len(all_stim_frs_by_trial['sound2']))])
+            # nontarget_discrim_auc=roc_auc_score(binary_label,vis2_vs_aud2_frs)
+            # alignment_metrics['nontarget_discrim_roc_auc'].append(nontarget_discrim_auc)
+
+            # #vis vs. aud
+            # vis_and_aud_frs=np.concatenate([all_stim_frs_by_trial['vis1'].values,all_stim_frs_by_trial['vis2'].values,
+            #                                 all_stim_frs_by_trial['sound1'].values,all_stim_frs_by_trial['sound2'].values])
+            # binary_label=np.concatenate([np.ones(len(all_stim_frs_by_trial['vis1'])+len(all_stim_frs_by_trial['vis2'])),
+            #                             np.zeros(len(all_stim_frs_by_trial['sound1'])+len(all_stim_frs_by_trial['sound2']))])
+            # vis_vs_aud_auc=roc_auc_score(binary_label,vis_and_aud_frs)
+            # alignment_metrics['vis_vs_aud_roc_auc'].append(vis_vs_aud_auc)
+
+            #lick modulation
+            if "DynamicRouting" in session_info.project:
+                lick_trials = trials.query('(stim_name=="vis1" and context_name=="aud" and is_response==True) or \
+                                        (stim_name=="sound1" and context_name=="vis" and is_response==True)')
+                non_lick_trials = trials.query('(stim_name=="vis1" and context_name=="aud" and is_response==False) or \
+                                                (stim_name=="sound1" and context_name=="vis" and is_response==False)')
+            elif "Templeton" in session_info.project:
+                lick_trials = trials.query('is_response==True')
+                non_lick_trials = trials.query('is_response==False')
+
+            lick_frs_by_trial = trial_da.sel(unit_id=unit['unit_id'],time=slice(0.2,0.5),trials=lick_trials.index).mean(dim='time',skipna=True)
+            non_lick_frs_by_trial = trial_da.sel(unit_id=unit['unit_id'],time=slice(0.2,0.5),trials=non_lick_trials.index).mean(dim='time',skipna=True)
+
+            #ROC AUC
+            binary_label = np.concatenate([np.ones(lick_frs_by_trial.size),np.zeros(non_lick_frs_by_trial.size)])
+            binary_score = np.concatenate([lick_frs_by_trial.values,non_lick_frs_by_trial.values])
+            if len(np.unique(binary_label))>1:
+                lick_roc_auc = roc_auc_score(binary_label, binary_score)
+            else:
+                lick_roc_auc = np.nan
+            alignment_metrics['lick_modulation_roc_auc'].append(lick_roc_auc)
+        
+        else:
+            alignment_metrics['any_vis_roc_auc'].append(np.nan)
+            alignment_metrics['any_aud_roc_auc'].append(np.nan)
+            alignment_metrics['vis_discrim_roc_auc'].append(np.nan)
+            alignment_metrics['aud_discrim_roc_auc'].append(np.nan)
+            alignment_metrics['lick_modulation_roc_auc'].append(np.nan)
+    
+    alignment_metrics = pd.DataFrame(alignment_metrics)
+    alignment_metrics['visual_response'] = np.abs(alignment_metrics['any_vis_roc_auc'] - 0.5)*2
+    alignment_metrics['auditory_response'] = np.abs(alignment_metrics['any_aud_roc_auc'] - 0.5)*2
+    alignment_metrics['visual_discrim'] = np.abs(alignment_metrics['vis_discrim_roc_auc'] - 0.5)*2
+    alignment_metrics['auditory_discrim'] = np.abs(alignment_metrics['aud_discrim_roc_auc'] - 0.5)*2
+    alignment_metrics['lick_modulation'] = np.abs(alignment_metrics['lick_modulation_roc_auc'] - 0.5)*2
+
+
+    alignment_metrics.drop(columns=['any_vis_roc_auc','any_aud_roc_auc','vis_discrim_roc_auc','aud_discrim_roc_auc','lick_modulation_roc_auc'],inplace=True)
+
+    probes=alignment_metrics['probe'].unique()
+    for probe in probes:
+        probe_units=alignment_metrics.query('probe==@probe')
+        probe_units.to_csv(os.path.join(save_path,session_info.id+'_day_'+str(session_info.experiment_day)+'_'+probe+'_stim_modulation.csv'),index=False)
+
