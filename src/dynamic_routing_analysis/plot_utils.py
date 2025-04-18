63d5ea4345650bf7dcf0913960ac58a63a4d83a2
bjhardcastle <ben.hardcastle@alleninstitute.org>
2025-04-18 14:12:09 -0700

    Add cmap parameter to altair ccf plot

--- src/dynamic_routing_analysis/plot_utils.py
+++ src/dynamic_routing_analysis/plot_utils.py
@@ -1946,7 +1946,7 @@ def plot_brain_heatmap(
         ax.set_clip_on(False)
 
     if interactive:
-        chart = plot_gdf_alt(gdfs, ccf_colors=False)
+        chart = plot_gdf_alt(gdfs, ccf_colors=False, cmap=cmap)
         return chart, tuple(gdfs)
     else:
         return fig, tuple(gdfs)
@@ -1955,6 +1955,7 @@ def plot_brain_heatmap(
 def plot_gdf_alt(
     gdfs: gpd.GeoDataFrame | Iterable[gpd.GeoDataFrame],
     ccf_colors: bool = False,
+    cmap: str = "viridis",
     value_name: str = "value",
 ) -> alt.Chart:
     if isinstance(gdfs, gpd.GeoDataFrame):
@@ -2039,7 +2040,7 @@ def plot_gdf_alt(
             color = alt.Color(
                 "value:Q",
                 title=value_name,
-                scale=alt.Scale(scheme="viridis"),
+                scale=alt.Scale(scheme=cmap.lower()),
                 legend=alt.Legend(orient="bottom", direction="horizontal"),
                 # condition=condition,
             )
