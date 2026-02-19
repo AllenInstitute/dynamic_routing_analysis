728d04a3f8e2309089a06a32ebff7525816d6de7
egmcbride <ethan.g.mcbride@gmail.com>
2026-02-18 16:11:42 -0800

    add functions for loading and querying decoder results from s3 with polars

--- src/dynamic_routing_analysis/decoding_utils.py
+++ src/dynamic_routing_analysis/decoding_utils.py
@@ -13,6 +13,7 @@ import time
 import npc_lims
 import numpy as np
 import pandas as pd
+import polars as pl
 import upath
 import zarr
 from sklearn.metrics import balanced_accuracy_score
@@ -482,3 +483,317 @@ def decoder_helper(input_data,labels,decoder_type='linearSVC',crossval='5_fold',
     return output
 
 
+def get_multi_probe_expr(combine_multi_probe_rec):
+    # creates a polars expression to toggle whether to load structure results combined across probe insertions, or preserve individual probe insertion results
+    if combine_multi_probe_rec:
+        return pl.col('electrode_group_names').list.len().gt(1) | pl.col('is_sole_recording').eq(True)
+    else:
+        return pl.col('electrode_group_names').list.len().eq(1) | pl.col('is_sole_recording').eq(True)
+
+
+def get_structure_grouping(keep_original_structure=False):
+    # structure grouping needed when merging columns from units table to decoder results
+    structure_grouping = {
+        'SCop': 'SCs',
+        'SCsg': 'SCs',
+        'SCzo': 'SCs',
+        'SCig': 'SCm',
+        'SCiw': 'SCm',
+        'SCdg': 'SCm',
+        'SCdw': 'SCm',
+        "ECT1": 'ECT',
+        "ECT2/3": 'ECT',    
+        "ECT6b": 'ECT',
+        "ECT5": 'ECT',
+        "ECT6a": 'ECT', 
+        "ECT4": 'ECT',
+    }
+    keep_original_structure = False
+    if keep_original_structure:
+        n_repeats = 2
+    else:
+        n_repeats = 1
+
+    return structure_grouping, n_repeats
+
+
+def exclude_structures_from_df(df, exclude_redundant_structures=True, exclude_general_structures=True):
+    # exclude redundant structures and/or general structures from decoder results dataframe
+    # redundant structures are those that were oversplit due to how processing code assigned structure labels
+    # general structures are those in the CCF "in between" more specific structures (or ventricles/fiber tracts). These should be excluded by default.
+    redundant_structures =['SCop', 'SCsg', 'SCzo', 'SCig', 'SCiw', 'SCdg', 'SCdw', 'ECT1', 'ECT2/3', 'ECT4', 'ECT5', 'ECT6a', 'ECT6b']
+    general_structures =['CTXsp', 'STR', 'PAL', 'TH', 'HY', 'MB', 'P', 'MY', 'CB', 'VL', 'V3', 'V4', 'SEZ', 
+                         'fiber tracts', 'scwm', 'root', 'lot', 'out of brain', 'undefined']
+
+    if exclude_redundant_structures:
+        df = df.filter(~pl.col('structure').is_in(redundant_structures))
+    if exclude_general_structures:
+        df = df.filter(~pl.col('structure').is_in(general_structures),)
+        #also remove any structure beginning with lowercase letter - indicating fiber tracts or out of brain
+        unique_structures=df['structure'].unique()
+        lowercase_structures=[]
+        for ss in unique_structures:
+            if ss[0].islower():
+                lowercase_structures.append(ss)
+        if len(lowercase_structures)>0:
+            df = df.filter(~pl.col('structure').is_in(lowercase_structures),)
+
+    return df
+
+def load_single_session_decoder_accuracy(results_path, sel_session, combine_multi_probe_rec=True):
+    
+    #define grouping columns
+    grouping_cols = {
+        'session_id',
+        'structure',
+        'electrode_group_names',
+        'unit_subsample_size',
+        'bin_size',
+        'bin_center',
+    }
+    
+    combine_multi_probe_expr = get_multi_probe_expr(combine_multi_probe_rec)
+
+    example_session_df = (
+        pl.scan_parquet(results_path)
+        .with_columns(
+            pl.col('electrode_group_names').flatten().n_unique().eq(1).over(grouping_cols - {'electrode_group_names'}).alias('is_sole_recording'),
+        )
+        .filter(
+            pl.col('session_id').eq(sel_session),
+            combine_multi_probe_expr,
+            pl.col('is_all_trials').not_(),
+        )
+        .sort('shift_idx')
+        .group_by(
+            grouping_cols - {'electrode_group_names'}| {'repeat_idx'}, 
+            maintain_order=True,
+        )
+        .agg(
+            pl.col('balanced_accuracy_test', 'shift_idx'),
+        ).sort('unit_subsample_size','repeat_idx')
+        .collect()
+    )
+
+    return example_session_df
+
+
+
+def load_structure_average_decoder_accuracy(results_path, session_list, combine_multi_probe_rec=True, exclude_redundant_structures=True, exclude_general_structures=True):
+
+    #define grouping columns
+    grouping_cols = {
+        'session_id',
+        'structure',
+        'electrode_group_names',
+        'unit_subsample_size',
+        'bin_size',
+        'bin_center',
+    }
+
+    combine_multi_probe_expr = get_multi_probe_expr(combine_multi_probe_rec)
+
+    # structure-wise average decoding accuracy
+    results_df = (
+        pl.scan_parquet(results_path)
+        .filter(
+            pl.col('session_id').is_in(session_list),
+        )
+        .with_columns(
+            pl.col('electrode_group_names').flatten().n_unique().eq(1).over(grouping_cols - {'electrode_group_names'}).alias('is_sole_recording'),
+        )
+        .filter(
+            combine_multi_probe_expr,
+            pl.col('is_all_trials').not_(),
+            pl.col('session_id').n_unique().ge(1).over('structure', 'unit_subsample_size')#, 'unit_criteria'),
+        )
+        
+        # get the means for each recording over repeats:
+        .group_by(grouping_cols | {'shift_idx'}, maintain_order=True)
+        .agg(
+            pl.col('balanced_accuracy_test').mean(), # over repeats
+        )
+        # get the aligned result and median over shifts:
+        .group_by(grouping_cols)
+        .agg(
+            pl.col('balanced_accuracy_test').filter(pl.col('shift_idx') == 0).first().alias('mean_true'),
+            pl.col('balanced_accuracy_test').filter(pl.col('shift_idx') != 0).median().alias('median_null'),
+        )
+        # get the difference between true and null:
+        .with_columns(
+            pl.col('mean_true').sub(pl.col('median_null')).alias('mean_diff'),
+        )
+        # get the means over sessions:
+        .group_by('structure', 'unit_subsample_size','bin_size','bin_center')#, 'unit_criteria')
+        .agg(
+            pl.col('mean_true').mean(),
+            pl.col('mean_true').std().truediv(pl.col('mean_true').count().pow(0.5)).alias('sem_true'),
+            pl.col('median_null').mean(),
+            pl.col('median_null').std().truediv(pl.col('mean_true').count().pow(0.5)).alias('sem_null'),
+            pl.col('mean_diff').mean(),
+            pl.col('mean_diff').std().truediv(pl.col('mean_true').count().pow(0.5)).alias('sem_diff'),
+            pl.col('session_id').n_unique().alias('num_sessions')
+        )
+        .with_columns(
+            pl.col('num_sessions').cast(pl.Int64),
+        )
+        .sort(pl.col('mean_diff').mean().over('structure'), descending=True)
+        .collect()
+    )
+
+    results_df = exclude_structures_from_df(
+        results_df, 
+        exclude_redundant_structures=exclude_redundant_structures, 
+        exclude_general_structures=exclude_general_structures
+    )
+
+    return results_df
+
+
+def load_session_wise_decoder_accuracy(
+        results_path, session_list, session_table, 
+        combine_multi_probe_rec=True, keep_original_structure=False, 
+        exclude_redundant_structures=True, exclude_general_structures=True):
+
+    #define grouping columns
+    grouping_cols = {
+        'session_id',
+        'structure',
+        'electrode_group_names',
+        'unit_subsample_size',
+        'bin_size',
+        'bin_center',
+    }
+    
+    combine_multi_probe_expr = get_multi_probe_expr(combine_multi_probe_rec)
+    structure_grouping, n_repeats = get_structure_grouping(keep_original_structure)
+
+    #add total n units, cross-modal dprime, n good blocks?
+    results_session_df = (
+        pl.scan_parquet(results_path)
+        .filter(
+            pl.col('session_id').is_in(session_list),
+        )
+        .with_columns(
+            pl.col('electrode_group_names').flatten().n_unique().eq(1).over(grouping_cols - {'electrode_group_names'}).alias('is_sole_recording'),
+        )
+        .filter(
+            combine_multi_probe_expr,
+            pl.col('is_all_trials').not_(),
+        )
+        #get total n units
+        .join(
+            other=(
+                pl.scan_parquet('s3://aind-scratch-data/dynamic-routing/cache/nwb_components/v0.0.272/consolidated/units.parquet')
+                .with_columns(
+                    pl.col('session_id').str.split('_').list.slice(0, 2).list.join('_'),
+                )
+                #make new rows according to structure_grouping
+                .with_columns(
+                    pl.when(pl.col('structure').is_in(structure_grouping.keys()))
+                    .then(pl.col('structure').repeat_by(n_repeats))
+                    .otherwise(pl.col('structure').repeat_by(1))
+                )
+                .explode('structure')
+                .with_columns(
+                    pl.when(pl.col('structure').is_in(structure_grouping.keys()).is_first_distinct().over('unit_id'))
+                    .then(pl.col('structure').replace(structure_grouping))
+                    .otherwise(pl.col('structure'))
+                )
+                .group_by('session_id','structure')
+                .agg(
+                    pl.col('unit_id').len().alias('total_n_units')
+                )
+            ),
+            on=['session_id','structure'],
+            how='left',
+        )
+        #join on session table to get cross-modal dprime, etc.
+        .join(
+            other=session_table.filter(
+                pl.col('session_id').is_in(session_list)
+            ).select(
+                'session_id',
+                'n_passing_blocks',
+                'cross_modality_dprime_vis_blocks',
+                'cross_modality_dprime_aud_blocks',
+            ).lazy(),
+            on='session_id',
+            how='left',
+        )
+        # get the means for each recording over repeats:
+        .group_by(grouping_cols | {'shift_idx', 'n_passing_blocks', 'cross_modality_dprime_vis_blocks', 
+                                'cross_modality_dprime_aud_blocks', 'total_n_units'}, maintain_order=True)
+        .agg(
+            pl.col('balanced_accuracy_test').mean(), # over repeats
+        )
+        # get the aligned result and median over shifts:
+        .group_by(grouping_cols - {'electrode_group_names'} | {'n_passing_blocks', 'cross_modality_dprime_vis_blocks', 
+                                                            'cross_modality_dprime_aud_blocks', 'total_n_units'})
+        .agg(
+            pl.col('balanced_accuracy_test').filter(pl.col('shift_idx') == 0).first().alias('mean_true'),
+            pl.col('balanced_accuracy_test').filter(pl.col('shift_idx') != 0).median().alias('median_null'),
+            pl.col('balanced_accuracy_test', 'shift_idx').sort_by('shift_idx'),
+        )
+        # get the difference between true and null:
+        .with_columns(
+            pl.col('mean_true').sub(pl.col('median_null')).alias('mean_diff'),
+        )
+        .sort('session_id', 'structure', 'unit_subsample_size', descending=False)
+        .collect()
+    )
+
+    results_session_df = exclude_structures_from_df(
+        results_session_df, 
+        exclude_redundant_structures=exclude_redundant_structures, 
+        exclude_general_structures=exclude_general_structures
+    )
+
+    return results_session_df
+
+
+
+def get_average_session_structure_ccf_coords(results_session_df,all_units_table_path=None):
+
+    if type(results_session_df) == pd.DataFrame or type(results_session_df) == pl.DataFrame:
+        if type(results_session_df) == pl.DataFrame:
+            results_session_df = results_session_df.to_pandas()
+    else:
+        raise ValueError('results_session_df must be a pandas or polars DataFrame')
+            
+    if all_units_table_path is None:
+        all_units_table_path='s3://aind-scratch-data/dynamic-routing/cache/nwb_components/v0.0.272/consolidated/units.parquet'
+    all_units_table=pd.read_parquet(all_units_table_path)
+
+    session_structure_ccf_coords={
+    'session_id':[],
+    'structure':[],
+    'ccf_dv':[],
+    'ccf_ml':[],
+    'ccf_ap':[]
+    }
+
+    for session_id in results_session_df['session_id'].unique():
+        session_df=results_session_df.query('session_id==@session_id')
+        for structure in session_df['structure'].unique():
+            #get SCm, SCs
+            if structure=='SCs':
+                structure_query='(structure=="SCop" | structure=="SCsg" | structure=="SCzo")'
+            elif structure=='SCm':
+                structure_query='(structure=="SCig" | structure=="SCiw" | structure=="SCdg" | structure=="SCdw")'
+            else:
+                structure_query='structure==@structure'
+            session_structure_units=all_units_table.query('session_id==@session_id & '+structure_query)
+            ccf_dv=np.nanmean(session_structure_units['ccf_dv'].values)
+            ccf_ml=np.nanmean(session_structure_units['ccf_ml'].values)
+            ccf_ap=np.nanmean(session_structure_units['ccf_ap'].values)
+
+            session_structure_ccf_coords['session_id'].append(session_id)
+            session_structure_ccf_coords['structure'].append(structure)
+            session_structure_ccf_coords['ccf_dv'].append(ccf_dv)
+            session_structure_ccf_coords['ccf_ml'].append(ccf_ml)
+            session_structure_ccf_coords['ccf_ap'].append(ccf_ap)
+
+    session_structure_ccf_coords_df=pd.DataFrame(session_structure_ccf_coords)
+
+    return session_structure_ccf_coords_df
